e26e3f25061a99bdd726b306f5dd22a0dd1d1a96
Russell Hay <russell.hay@gmail.com>
2018-10-11 15:37:35 -0700

    Add error type

--- src/ftdi2xx.rs
+++ src/ftdi2xx.rs
@@ -1,6 +1,36 @@
+use std::result;
+use std::fmt;
+use std::error::Error;
 use std::alloc::{alloc, Layout};
 use std::mem::{size_of, transmute};
 use super::native::*;
+
+#[derive(Debug)]
+enum FtdiError {
+    Generic,
+    UnableToAllocate,
+    NoDevicesFound,
+}
+impl Error for FtdiError {
+    fn description(&self) -> &str {
+        match self {
+            FtdiError::Generic => "Generic FTDI Error",
+            FtdiError::UnableToAllocate => "Unable to allocated needed memory",
+            FtdiError::NoDevicesFound => "No FDTI devices detected",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
+}
+impl fmt::Display for FtdiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
+
+type Result<T> = result::Result<T, FtdiError>;
 
 struct Ftdi2xx {}
 
@@ -17,17 +47,21 @@ impl Ftdi2xx {
         println!("Allocated {:?}\n{:?}", alloc_size, data);
     }
 
-    fn get_device_list() {
+    fn get_device_list() -> Result<Vec<_ft_device_list_info_node>> {
         unsafe {
             let mut num_devs: u32 = 0;
             let mut status: FT_STATUS = FT_CreateDeviceInfoList(&mut num_devs);
             println!("Found {:?} device(s)", num_devs);
+            if num_devs == 0 {
+                return Err(FtdiError::NoDevicesFound)
+            }
             Ftdi2xx::allocate_list_info_node(num_devs as usize);
 
             // assert!(status == FT_OK);
             // status = FT_GetDeviceInfoList(&mut info_node, &mut num_devs);
             // println!("{:?} = {:?}", status, num_devs);
         }
+        Err(FtdiError::Generic)
     }
 }
 
@@ -37,6 +71,6 @@ mod test {
 
     #[test]
     fn test_get_device_list() {
-        Ftdi2xx::get_device_list();
+        Ftdi2xx::get_device_list().unwrap();
     }
 }
--- src/native.rs
+++ src/native.rs
@@ -4,13 +4,11 @@
 #![allow(dead_code)]
 
 use std::fmt;
-use std::ffi::CStr;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 impl fmt::Debug for _ft_device_list_info_node {
     fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Device({} {:?})", self.ID, self.SerialNumber);
-        Ok(())
+        write!(f, "Device({} {:?})", self.ID, self.SerialNumber)
     }
 }
